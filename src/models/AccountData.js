334aec42a4c71a24880c780518f0d05b9e27519c
Murilo Rezende <murilorez79@hotmail.com>
2023-05-08 16:45:11 -0300

    Adição do email no Schema

--- src/models/AccountData.js
+++ src/models/AccountData.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const AccountSchema = new mongoose.Schema({
     username: String,
     password: String,
+    email: String,
 });
 
 module.exports = mongoose.model('Accounts', AccountSchema);
