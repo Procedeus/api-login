47ebdc9c7132078aa956192cd37540e9d7c9cf9d
Murilo Rezende <murilorez79@hotmail.com>
2023-05-08 16:46:54 -0300

    Adição da rota de informações do usuário

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const routes = require('./routes');
 const app = express();
 const cors = require('cors');
 require('./config/dbConfig');
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ const routes = express.Router();
 
 const UserController = require('./controllers/UserController');
 
+routes.post('/login', UserController.login);
 routes.post('/signup', UserController.signup);
-routes.post('/signin', UserController.signin);
+routes.get('/user', UserController.verifyToken, UserController.searchUser);
 
 module.exports = routes;
